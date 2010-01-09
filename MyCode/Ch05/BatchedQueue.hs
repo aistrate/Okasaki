04d5d493c8bc8affea7b6218580878c41b3d362b
Adrian Istrate <adr_istrate@yahoo.com>
2010-01-09 22:26:37 +0100

    Chapter 5: BatchedQueue

---
+++ MyCode/Ch05/BatchedQueue.hs
@@ -0,0 +1,24 @@
+module BatchedQueue (module Queue, BatchedQueue) where
+  
+import Prelude hiding (head, tail)
+import Queue
+
+
+data BatchedQueue a = BQ [a] [a]
+
+
+check [] r = BQ (reverse r) []
+check f r = BQ f r
+
+
+instance Queue BatchedQueue where
+  empty = BQ [] []
+  isEmpty (BQ f r) = null f
+  
+  snoc (BQ f r) x = check f (x:r)
+  
+  head (BQ [] _) = error "BatchedQueue.head: empty queue"
+  head (BQ (x:f) r) = x
+  
+  tail (BQ [] _) = error "BatchedQueue.tail: empty queue"
+  tail (BQ (x:f) r) = check f r
